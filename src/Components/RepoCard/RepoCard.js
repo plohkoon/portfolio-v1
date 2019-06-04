8c4fc2f829ab3eabf5dc996128429d4cff3cb5c9
plohkoon <greg.huber@outlook.com>
2019-06-03 20:19:30 -0700

    fully implemented the "look" of the repos div including a rotation in R3

--- src/Components/RepoCard/RepoCard.js
+++ src/Components/RepoCard/RepoCard.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import './repocard.css';
 
 function RepoCard(props) {
-
+  //initializes values for dimensions of div
+  const [middle, setMiddle] = useState();
+  const [dim, setDim] = useState();
+  //sets the rotation to be applied to div
+  const [rot, setRot] = useState({"x": 0, "y": 0});
+  const cardDiv = useRef();
+  //handler for mousemove
   const onMouseMove = e => {
-    let coords = {
-      "x": e.nativeEvent.offsetX,
-      "y": e.nativeEvent.offsetY
+    let divBound = e.target.getBoundingClientRect();
+    //extracts coords from events
+    let x = e.clientX - divBound.left,
+        y = e.clientY - divBound.top;
+    //calculates the x and y rotation in turns
+    let rotation = {
+      "x": (x-middle.x)/(dim.x/2),
+      "y": (y-middle.y)/(dim.y/2)
     }
-    console.log(coords);
+    //sets the state
+    setRot(rotation);
   }
-
+  //resets rotation when Mouse leaves
+  const onMouseLeave = e => {
+    setRot({"x": 0, "y": 0});
+  }
+  //gets the size of the card
+  useEffect(() => {
+    let height=cardDiv.current.offsetHeight,
+        width=cardDiv.current.offsetWidth;
+    setMiddle({"x": width/2, "y": height/2});
+    setDim({"x": width, "y": height})
+  }, [])
+  //renders the repo card
   return (
     <div
       className="repo"
       onMouseMove={onMouseMove}
+      onMouseLeave={onMouseLeave}
+      ref={cardDiv}
+      style={{
+        transform: `rotate3d(${rot.y}, ${-1*rot.x}, 0, 0.025turn)`
+      }}
     >
       <h1>{props.data.name.toUpperCase()}</h1>
       <p>{props.data.description}</p>
