db81653db5a8b59ce94cc5ac69a6e8cffcc334e1
plohkoon <greg.huber@outlook.com>
2019-07-25 12:39:26 -0700

    Changed repo card to hexcards that tile like a honey comb. removed logic for the mouse over effect as it was causing lag. Added language fetching

--- src/Components/RepoCard/RepoCard.js
+++ src/Components/RepoCard/RepoCard.js
@@ -1,57 +1,20 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect } from 'react';
 
 import './repocard.css';
 
 function RepoCard(props) {
-  //initializes values for dimensions of div
-  const [middle, setMiddle] = useState();
-  const [dim, setDim] = useState();
-  //sets the rotation to be applied to div
-  const [rot, setRot] = useState({"x": 0, "y": 0});
-  //sets the max for the shadow when the mouse is all the way out
-  const shadowMax = 7;
-  //sets a ref to refer to the cardDiv for height and width measurements
-  const cardDiv = useRef();
-  //handler for mousemove
-  const onMouseMove = e => {
-    let divBound = e.target.getBoundingClientRect();
-    //extracts coords from events
-    let x = e.clientX - divBound.left,
-        y = e.clientY - divBound.top;
-    //calculates the x and y rotation in turns
-    let rotation = {
-      "x": (x-middle.x)/(dim.x/2),
-      "y": (y-middle.y)/(dim.y/2)
-    }
-    //sets the state
-    setRot(rotation);
-  }
-  //resets rotation when Mouse leaves
-  const onMouseLeave = e => {
-    setRot({"x": 0, "y": 0});
-  }
-  //gets the size of the card
+  const [lang, setLang] = useState([]);
   useEffect(() => {
-    let height=cardDiv.current.offsetHeight,
-        width=cardDiv.current.offsetWidth;
-    setMiddle({"x": width/2, "y": height/2});
-    setDim({"x": width, "y": height})
-  }, [])
-  //renders the repo card
+    fetch(props.data.languages_url)
+      .then(res => res.json())
+      .then(data => Object.keys(data))
+      .then(data => setLang(data));
+  }, [props.data.languages_url]);
   return (
-    <div
-      className="repo"
-      onMouseMove={onMouseMove}
-      onMouseLeave={onMouseLeave}
-      ref={cardDiv}
-      style={{
-        transform: `rotate3d(${-1*rot.y}, ${rot.x}, 0, 0.025turn)`,
-        boxShadow: `${rot.x*shadowMax}px ${rot.y*shadowMax}px ${shadowMax}px grey`
-      }}
-    >
+    <a className="hexagon" href={props.data.html_url}>
       <h1>{props.data.name.toUpperCase()}</h1>
       <p>{props.data.description}</p>
-    </div>
+    </a>
   )
 }
 
