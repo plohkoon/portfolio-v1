9c78e8c9699a51fa57249e921400800b192e33be
plohkoon <greg.huber@outlook.com>
2019-08-09 15:25:23 -0700

    converted to using nth-of-type instead of nth-child, adjusted logic to be around hexagons

--- src/Components/RepoCard/RepoCard.js
+++ src/Components/RepoCard/RepoCard.js
@@ -1,21 +1,38 @@
 import React, {useState, useEffect } from 'react';
 
+import LangDisplay from '../LangDisplay/LangDisplay.js'
+
 import './repocard.css';
 
 function RepoCard(props) {
   const [lang, setLang] = useState([]);
+  const [hover, setHover] = useState(false);
   useEffect(() => {
     fetch(props.data.languages_url)
       .then(res => res.json())
       .then(data => Object.keys(data))
       .then(data => setLang(data));
   }, [props.data.languages_url]);
-  return (
-    <a className="hexagon" href={props.data.html_url}>
-      <h1>{props.data.name.toUpperCase()}</h1>
-      <p>{props.data.description}</p>
-    </a>
-  )
+  if(props.display) {
+    return (
+      <a
+        className={props.display ? "hexagon" : "hexisgone"}
+        href={props.data.html_url}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <h1>{props.data.name.toUpperCase()}</h1>
+        <p>{props.data.description}</p>
+        <LangDisplay langs={lang} />
+      </a>
+    )
+  }
+  else {
+    return (
+      <span name={props.data.name + " placeholder"}></span>
+    )
+  }
+  
 }
 
 export default RepoCard;
