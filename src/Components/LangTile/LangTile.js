e19ac58c3d3855f5bdbcd885ca569fb8ef916cec
plohkoon <greg.huber@outlook.com>
2019-08-13 09:42:08 -0700

    Added cases for more languages, fixed crash if language is not yet implemented

--- src/Components/LangTile/LangTile.js
+++ src/Components/LangTile/LangTile.js
@@ -5,6 +5,9 @@ import './langtile.css';
 const csslogo = require('./csslogo.png');
 const jslogo = require('./jslogo.png');
 const htmllogo = require('./htmllogo.png');
+const dartlogo = require('./dart.svg');
+const objclogo = require('./objclogo.png');
+const javalogo = require('./javalogo.png');
 
 function LangTile(props) {
   switch (props.lang) {
@@ -32,8 +35,34 @@ function LangTile(props) {
         </div>
       )
       break;
+    case 'Dart':
+      return (
+        <div className="DART lang">
+          <img src={dartlogo} alt="dart logo"/>
+          <h6>DART</h6>
+        </div>
+      )
+    case 'Objective-C':
+      return (
+        <div className="OBJC lang">
+          <img src={objclogo} alt="objective c logo"/>
+          <h6>OBJECTIVE C</h6>
+        </div>
+      )
+    case 'Java':
+      return (
+        <div className="JAVA lang">
+          <img src={javalogo} alt="java logo"/>
+          <h6>JAVA</h6>
+        </div>
+      )
     default:
       break;
+      return (
+        <div className='lang'>
+          <h6>{props.lang.toUpperCase()}</h6>
+        </div>
+      );
   }
 }
 
